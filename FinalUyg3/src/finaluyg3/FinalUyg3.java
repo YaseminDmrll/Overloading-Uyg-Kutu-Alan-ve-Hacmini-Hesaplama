6291a2e27d751ca4d2cc1f3ce5913fdf5f1043ec
Yasemin DEMİREL <65770236+YaseminDmrll@users.noreply.github.com>
2020-05-22 21:56:05 +0300

    Add files via upload

---
+++ FinalUyg3/src/finaluyg3/FinalUyg3.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package finaluyg3;
+
+/**
+ *
+ * @author Bora
+ */
+public class FinalUyg3 {
+
+   public void kutu(int en ,int boy)//kutu adında 2 parametreli metot oluşturdum.
+   {
+       System.out.println("En: "+en);//Ekrana en değerini yazdırdım.
+       System.out.println("Boy: "+boy);//Ekrana boy değerini yazdırdım.
+       System.out.println("Alan: "+(en*boy));//Ekrana alan değerini hesaplatıp değerini yazdırdım.
+   }
+   public void kutu(int en ,int boy,int kalinlik)//kutu adında 3 parametreli metot oluşturdum.
+   {
+       System.out.println("En: "+en);//Ekrana en değerini yazdırdım.
+       System.out.println("Boy: "+boy);//Ekrana boy değerini yazdırdım.
+       System.out.println("Kalınlık: "+kalinlik);//Ekrana kalınlık değerini yazdırdım.
+       System.out.println("Hacim: "+(en*boy*kalinlik));//Ekrana hacim değerini hesaplatıp yazdırdım.
+   }
+    public static void main(String[] args) {
+        FinalUyg3 nesne =new FinalUyg3();//nesne adında nesne oluşturduk.
+        nesne.kutu(5,10);//1. metouma parametre değerlerini verdim.
+        System.out.println("-----------------");
+        nesne.kutu(5, 10,5);//2. metouma parametre değerlerini verdim.
+    }
+    
+}
