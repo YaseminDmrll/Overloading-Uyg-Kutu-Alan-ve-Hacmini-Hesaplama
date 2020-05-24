3c18104d8a9e054c4b89e42929727da265f9867b
Yasemin DEMİREL <65770236+YaseminDmrll@users.noreply.github.com>
2020-05-24 14:24:23 +0300

    Update FinalUyg3.java

--- FinalUyg3/src/finaluyg3/FinalUyg3.java
+++ FinalUyg3/src/finaluyg3/FinalUyg3.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+Metotların Aşırı Yüklenmesi Kutu Alan ve Hacmini Hesaplama Uygulaması 
+en,boy,kalınlık gibi değerlerin verildiği  iki metot içerisinde alan ve hacminin hesaplanıp 
+ekrana yazdırıldığı  bir overloading uygulamasıdır.
+*/
 
 package finaluyg3;
 
